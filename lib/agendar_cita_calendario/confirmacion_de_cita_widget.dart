ba899027481c36e7abf98a32f512c972ebaf56e1
alethiaimperia <alethiaimperia@gmail.com>
2022-11-08 17:43:48 -0600

    otro mas

--- lib/agendar_cita_calendario/confirmacion_de_cita_widget.dart
+++ lib/agendar_cita_calendario/confirmacion_de_cita_widget.dart
@@ -14,6 +14,7 @@ class ConfirmacionDeCitaWidget extends StatefulWidget {
   @override
   _ConfirmacionDeCitaWidgetState createState() =>
       _ConfirmacionDeCitaWidgetState();
+  //prueba
 }
 
 class _ConfirmacionDeCitaWidgetState extends State<ConfirmacionDeCitaWidget> {
